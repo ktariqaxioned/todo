390a2f4fc73284d5e5dc684939b6e5eab4e30174
tariqk <tariqk@axioned.com>
2025-06-17 14:19:20 +0530

    moved svg in seperate file

---
+++ src/components/icons/update-icon.jsx
@@ -0,0 +1,20 @@
+const UpdateIcon = () => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 512 512"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="none"
+      strokeLinecap="square"
+      strokeMiterlimit="10"
+      strokeWidth="44"
+      d="M465 127L241 384l-92-92m-9 93l-93-93m316-165L236 273"
+    ></path>
+  </svg>
+);
+export default UpdateIcon;
